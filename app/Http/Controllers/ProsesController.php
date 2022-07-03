774e128b650fa69769793e8911162706e04b2d24
andika-eka <putuandika387@gmail.com>
2022-07-03 15:14:33 +0800

    stemming

--- app/Http/Controllers/ProsesController.php
+++ app/Http/Controllers/ProsesController.php
@@ -9,56 +9,54 @@ use Sastrawi\Stemmer\StemmerFactory;
 class ProsesController extends Controller
 {
 
+    
+
     public function index()
     {
         // reading text file 
         $text = Storage::get('public/file.txt');
         // dd($text);
+        // stemming filtered words using sastrawi
+        
         $lowStr = strtolower($text);
         $stringClean = preg_replace('/[^a-z0-9]+/i', ' ', $lowStr);
         // text tokenization
-        $token = explode(' ', $stringClean);
+        $stemmerFactory = new StemmerFactory();
+        $stemmer = $stemmerFactory->createStemmer();
+   
+        $stemmed   = $stemmer->stem($stringClean);
+        $token = explode(' ', $stemmed);
         // dd($token);
-
+        
         // filtering using stopword
         $stopword = Storage::get('public/stopwords-id.txt');
         $stopword = explode("\n", $stopword);
 
         $filtered = array_diff($token, $stopword);
-        // dd($filtered);
+        dd($filtered);
 
         // counting frequency
         $count = array_count_values($filtered);
         // dd($count);
+        
+        
 
-        // stemming filtered words using sastrawi
-        $stemmerFactory = new StemmerFactory();
-        $stemmer = $stemmerFactory->createStemmer();
-        $stemmed = array_map(function ($item) use ($stemmer) {
-            return $stemmer->stem($item);
-        }, $filtered);
-        dd($stemmed);
+        
+        // dd($stemmed."\n");
 
 
 
-
-
-
-
-
-
-
-        // //weighting using TF-IDF
-        // $tf = array();
-        // $idf = array();
-        // $tfidf = array();
-        // $total = count($filtered);
-        // foreach ($stemmed as $key => $value) {
-        //     $tf[$key] = $count[$value] / $total;
-        //     $idf[$value] = log($total / $count[$value]);
-        //     $tfidf[$key] = $tf[$key] * $idf[$value];
-        // }
-        // dd($tfidf);
+        // weighting using TF-IDF
+        $tf = array();
+        $idf = array();
+        $tfidf = array();
+        $total = count($filtered);
+        foreach ($filtered as $key => $value) {
+            $tf[$key] = $count[$value] / $total;
+            $idf[$value] = log($total / $count[$value]);
+            $tfidf[$key] = $tf[$key] * $idf[$value];
+        }
+        dd($tfidf);
 
     }
 }
